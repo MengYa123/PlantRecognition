5cd7825dc8cb26b43e0c44a4684859944b486ce1
MengZhongyuan <395177422@qq.com>
2019-11-26 19:35:45 +0800

    initiate the project

---
+++ src/main/java/com/vegetablechicken/plantrecognition/PlantRecognitionApplication.java
@@ -0,0 +1,13 @@
+package com.vegetablechicken.plantrecognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantRecognitionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantRecognitionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vegetablechicken/plantrecognition/PlantRecognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vegetablechicken.plantrecognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantRecognitionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
